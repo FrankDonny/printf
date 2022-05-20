d1b10cf380578e2fe5b0b286680a1498ef2ebd53
FrankDonny <arkohfrank1@gmail.com>
2022-05-20 12:55:23 +0000

    printf function

--- main.h
+++ main.h
@@ -1,13 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
 
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int _printf(const char *format, ...);
+void _vprintf(const char *format, va_list list);
 void un_int_to_str(unsigned int figure, int base, char *buffer);
 void int_to_str(int figure, int base, char *buffer);
-void _vprintf(const char *format, va_list list);
-int _printf(const char *format, ...);
 void hex_X(unsigned int figure, int base, char *buffer);
 void format_specifier(const char *format, va_list list);
-char *_strRev(char *str);
+/*char *_strRev(char *str);*/
 
 #endif
